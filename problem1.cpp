d6407b2ce86696b11fac6b0ac11985bb8084c818
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-05-10 14:37:02 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -10,151 +10,127 @@ public:
     Node *right;
     int height;
 };
-
 int height(Node *node){
-    if(node == NULL) return 0;
+    if(node==NULL) return 0;
     return node->height;
 }
-
 int getBalanceFactor(Node *node){
-    if(node == NULL) return 0;
-    return height(node->left) - height(node->right);
+    if(node==NULL) return 0;
+    return height(node->left)- height(node->right);
 }
-
 Node *newNode(int key){
-    Node *node = new Node();
-    node->key = key;
-    node->left = NULL;
-    node->right = NULL;
-    node->height = 1;
+    Node *node=new Node();
+    node->key=key;
+    node->left=NULL;
+    node->right=NULL;
+    node->height=1;
     return node;
 }
-
 Node *rotateRight(Node *x){
-    Node *y = x->left;
-    Node *t = y->right;
-    y->right = x;
-    x->left = t;
-    x->height = max(height(x->right), height(x->left)) + 1;
-    y->height = max(height(y->right), height(y->left)) + 1;
+    Node *y=x->left;
+    Node *t=y->right;
+    y->right=x;
+    x->left=t;
+    x->height=max(height(x->right), height(x->left))+1;
+    y->height=max(height(y->right), height(y->left))+1;
     return y;
 }
-
 Node *rotateLeft(Node *y){
-    Node *x = y->right;
-    Node *t = x->left;
-    x->left = y;
-    y->right = t;
-    y->height = max(height(y->left), height(y->right)) + 1;
-    x->height = max(height(x->left), height(x->right)) + 1;
+    Node *x=y->right;
+    Node *t=x->left;
+    x->left=y;
+    y->right=t;
+    y->height=max(height(y->left), height(y->right))+1;
+    x->height=max(height(x->left), height(x->right))+1;
     return x;
 }
-
 Node *insertNode(Node *node, int key){
-    if(node == NULL){
+    if(node==NULL){
         return newNode(key);
     }
-    if(key < node->key){
-        node->left = insertNode(node->left, key);
-    } else if(key > node->key){
-        node->right = insertNode(node->right, key);
-    } else {
+    if(key<node->key){
+        node->left= insertNode(node->left,key);
+    }else if(key>node->key){
+        node->right= insertNode(node->right,key);
+
+    }else{
         return node;
     }
-    node->height = 1 + max(height(node->left), height(node->right));
-    int balanceFactor = getBalanceFactor(node);
-    if(balanceFactor > 1){
-        if(key < node->left->key){
+    node->height=1+max(height(node->left), height(node->right));
+    int balanceFactor= getBalanceFactor(node);
+    if(balanceFactor>1){
+        if(key<node->left->key){
             return rotateRight(node);
-        } else if(key > node->left->key){
-            node->left = rotateLeft(node->left);
+        }else if(key>node->left->key){
+            node->left= rotateLeft(node->left);
             return rotateRight(node);
-        }
-    }
-    if(balanceFactor < -1){
-        if(key < node->right->key){
-            node->right = rotateRight(node->right);
+        } }
+    if(balanceFactor<-1){
+        if(key<node->right->key){
+            node->right= rotateRight(node->right);
             return rotateLeft(node);
-        } else if(key > node->right->key){
+        }else if(key>node->right->key){
             return rotateLeft(node);
-        }
-    }
+        } }
     return node;
 }
-
-Node *nodeWithMinimumValue(Node *node) {
+Node *nodeWithMimumValue(Node *node) {
     Node *current = node;
     while (current->left != NULL)
         current = current->left;
     return current;
 }
-
 Node *deleteNode(Node *node, int key){
-    if(node == NULL){
+    if(node==NULL){
         return node;
     }
-    if(key < node->key){
-        node->left = deleteNode(node->left, key);
-    } else if(key > node->key){
-        node->right = deleteNode(node->right, key);
-    } else {
-        if(node->right == NULL || node->left == NULL){
-            Node* temp = node->left ? node->left : node->right;
-            if(temp == NULL){
-                temp = node;
-                node = NULL;
-            } else {
-                *node = *temp;
-            }
+    if(key<node->key){
+        node->left= deleteNode(node->left,key);
+    }else if(key>node->key){
+        node->right= deleteNode(node->right,key);
+    }else{
+        if(node->right==NULL||node->left==NULL){
+            Node* temp=node->left?node->left:node->right;
+            if(temp==NULL){
+                temp=node;
+                node=NULL;
+            }else{
+                *node=*temp;}
             free(temp);
-        } else {
-            Node *temp = nodeWithMinimumValue(node->right);
+        }else{
+            Node *temp = nodeWithMimumValue(node->right);
             node->key = temp->key;
-            node->right = deleteNode(node->right, temp->key);
-        }
-    }
-    if (node == NULL){
+            node->right = deleteNode(node->right,temp->key);
+        } }
+    if (node==NULL){
         return node;
     }
-    node->height = 1 + max(height(node->left), height(node->right));
-    int balanceFactor = getBalanceFactor(node);
-    if(balanceFactor > 1){
-        if(getBalanceFactor(node->left) >= 0){
+    node->height=1+max(height(node->left), height(node->right));
+
+
+    int balanceFactor= getBalanceFactor(node);
+    if(balanceFactor>1){
+        if(getBalanceFactor(node->left)>=0){
             return rotateRight(node);
-        } else {
-            node->left = rotateLeft(node->left);
+        }else{
+            node->left= rotateLeft(node->left);
             return rotateRight(node);
         }
     }
-    if(balanceFactor < -1){
-        if(getBalanceFactor(node->right) <= 0){
+    if(balanceFactor<-1){
+        if(getBalanceFactor(node->right)<=0){
             return rotateLeft(node);
-        } else {
-            node->right = rotateRight(node->right);
+        }else{
+            node->right= rotateRight(node->right);
             return rotateLeft(node);
-        }
-    }
+        } }
     return node;
 }
-
-void printLevelOrder(Node* root) {
-    if (root == nullptr)
-        return;
-
-    queue<Node*> q;
-    q.push(root);
-
-    while (!q.empty()) {
-        int size = q.size();
-        while (size--) {
-            Node* current = q.front();
-            q.pop();
-            cout << current->key << " ";
-            if (current->left)
-                q.push(current->left);
-            if (current->right)
-                q.push(current->right);
-        }
+void printTree(Node *root) {
+    if (root != nullptr) {
+        cout << root->key <<" ";
+        printTree(root->left);
+        printTree(root->right);
     }
 }
 
@@ -170,10 +146,10 @@ int main() {
     }
 
     cin >> deleteValue;
-    root = deleteNode(root, deleteValue);
+    deleteNode(root, deleteValue);
 
    // cout << "Resulting tree in form of a queue: ";
-    printLevelOrder(root);
+    printTree(root);
     cout << endl;
 
     return 0;
